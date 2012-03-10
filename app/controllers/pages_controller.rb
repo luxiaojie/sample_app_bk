c7d38538b6598c54ca15fbec022f870a053b0099
lxj831 <lxj831@126.com>
2012-03-10 12:07:00 +0800

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+  def home
+	@title="Home"
+  end
+
+  def contact
+	@title="Contact"
+  end
+
+  def about
+
+  end
+
+  def help
+    
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'bc740b36447a0990c712e69199baadadeeb8098be1e798aa3cbe402f708e085768255d17ed583ca07e953cd44537e3ef7d7249d53cff4b115e5d44ada314a75e'
